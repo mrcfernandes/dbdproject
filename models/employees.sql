b9317a1afcd463ab8d5df9a3f1b2bef53383058f
marcos1995.ti@gmail.com <marcos1995.ti@gmail.com>
2026-07-18 13:46:11 +0000

    Versão 1.0

---
+++ models/employees.sql
@@ -0,0 +1,9 @@
+with calc_employess as (
+
+select 
+date_part(year,current_date) - date_part(year,birth_date) age,
+date_part(year,current_date) - date_part(year,hire_date) lenghtofservice,
+first_name + ' ' + last_name name, *
+from {{source('sources','employees')}}
+)
+select * from calc_employess
